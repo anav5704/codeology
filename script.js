d8b3ffa7555c2b224a93d4de28936071d74f7a3e
anav5704 <71007258+anav5704@users.noreply.github.com>
2023-03-13 09:34:16 +1200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let menu = document.getElementById("hamburgertoggle");
-let ham =  document.getElementById("btn");
-
-ham.addEventListener('click', () => {
-    menu.classList.toggle('activated');
-})
-
-let x =document.getElementById("followercount")
-x.innerHTML = " 680 ";
