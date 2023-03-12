c818552484405ea0d5b20ae7493d9f405d709c00
anav5704 <71007258+anav5704@users.noreply.github.com>
2023-03-13 09:27:38 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let menu = document.getElementById("hamburgertoggle");
+let ham =  document.getElementById("btn");
+
+ham.addEventListener('click', () => {
+    menu.classList.toggle('activated');
+})
+
+let x =document.getElementById("followercount")
+x.innerHTML = " 680 ";
