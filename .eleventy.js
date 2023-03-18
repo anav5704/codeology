1d56901bfaf8d642d0bff47f04114f24b00fefcd
Anav Chand <anavkki@gmail.com>
2023-03-18 15:11:35 +1200

    14

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/styles.css');
     eleventyConfig.addPassthroughCopy('./img');
     eleventyConfig.addPassthroughCopy('./src/script.js');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     return {
         dir: {
