904afc49786ac926218a91172a4787b6841a3759
Anav Chand <anavkki@gmail.com>
2023-04-05 13:49:06 +1200

    xml 3

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./img');
     eleventyConfig.addPassthroughCopy('./src/script.js');
     eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
 
     return {
         dir: {
