d9713bb747b9a729302ba7ef6baf3d2c5ce9d9c5
Anav Chand <anavkki@gmail.com>
2023-05-02 14:24:06 +1200

    hhh

--- public/script.js
+++ public/script.js
@@ -113,7 +113,8 @@ form.addEventListener('submit', e => {
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
         console.log("sent");
-        window.open(Freebie);
+        // window.open(Freebie);
+        location.href = (Freebie)
         closediv()
         form.reset();
     }).catch(error => console.error('Error!', error.message));
