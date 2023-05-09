954946a5c744f11a2c4a5ab8f15cfb83c887766e
Anav Chand <anavkki@gmail.com>
2023-05-09 22:15:40 +1200

    created test branch and added projects prototype

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/styles.css');
     eleventyConfig.addPassthroughCopy('./img');
     eleventyConfig.addPassthroughCopy('./src/script.js');
-    // eleventyConfig.addPassthroughCopy('./src/script2.js');
+    eleventyConfig.addPassthroughCopy('./src/script2.js');
     eleventyConfig.addPassthroughCopy('./src/admin');
     eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
     eleventyConfig.addPassthroughCopy('./Freebie.zip');
