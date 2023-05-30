0a1357aa88fa1ff521fde5155669562325f26f0c
Anav Chand <anavkki@gmail.com>
2023-05-31 11:02:28 +1200

    fixd some bugs

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ ham.addEventListener('click', () => {
 // Socials follower count varibles
 
 let x =document.getElementById("igfollowercount")
-x.innerHTML = " 2000 ";
+x.innerHTML = " 2k ";
 
 // Auto Change Year for Copyright
 
